5ee195018ae06d611b89485811633cf893223c32
tiago <tiago.ameller@gmail.com>
2018-12-27 13:31:52 +0100

    build epub manual

---
+++ docs/build.sh
@@ -0,0 +1,25 @@
+pandoc -o foundation_emails.epub title.txt \
+pages/alignment.md \
+pages/button.md \
+pages/callout.md \
+pages/compatibility.md \
+pages/css-guide.md \
+pages/gem-guide.md \
+pages/global.md \
+pages/grid.md \
+pages/index.md \
+pages/inky.md \
+pages/media-query.md \
+pages/menu.md \
+pages/migration.md \
+pages/panini.md \
+pages/sass-guide.md \
+pages/sass.md \
+pages/spacer.md \
+pages/styling.md \
+pages/thumbnail.md \
+pages/tips-tricks.md \
+pages/typography.md \
+pages/visibility.md \
+pages/wrapper.md \
+pages/zurb-stack.md 
